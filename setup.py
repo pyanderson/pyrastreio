c69341a1c799b562cf5fde9250cd94f2c589fa76
Anderson Lima <anderson.sl93@hotmail.com>
2018-01-12 19:23:07 -0300

    add license and update setup

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from setuptools import setup
 
 setup(
     name="pyrastreio",
-    version='0.0.1',
-    py_modules=['pyrastreio'],
+    version='0.0.3',
+    url='https://github.com/pyanderson/pyrastreio',
+    license='MIT License',
+    author='Anderson Lima',
+    author_email='anderson.sl93@hotmail.com',
+    keywords='correios rastreio encomenda compra',
+    description='Biblioteca de rastreio de encomendas no site do correios',
+    packages=['pyrastreio'],
     install_requires=['beautifulsoup4', 'requests', 'click', 'tabulate'],
     entry_points='''
         [console_scripts]
